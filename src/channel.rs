1a6dbd7e1805f5047e8456949033bbe0e8ad3af2
Sam Payson <scpayson@gmail.com>
2015-03-08 05:02:33 -0700

    Added the channel module.

--- src/channel.rs
+++ src/channel.rs
@@ -11,5 +11,19 @@
 // You should have received a copy of the GNU General Public License along with Gyroscope.  If not,
 // see <http://www.gnu.org/licenses/>.
 
-/// Channels for input and output from synth nodes.
-pub mod channel;
+/// A channel which reads `f32`s.
+pub trait In {
+    /// Read from this slice of `f32`s.
+    fn from_f32(&mut self, src: &[f32]);
+}
+
+/// A channel which writes `f32`s.
+pub trait Out {
+    /// Return number of `f32` samples that the next call to `to_f32` will produce.
+    fn num_samples(&self) -> usize;
+
+    /// Populate `dst` with data from the channel, return the number of `f32`s written, if `dst` is
+    /// big enough this will be the same number as is returned by a call to `self.num_samples()`
+    /// beforehand.
+    fn to_f32(&self, dst: &mut [f32]) -> usize;
+}
